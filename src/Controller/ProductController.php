61be57bfae9cea8ea483f21833d1d54767d2ca1f
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-03-24 21:06:59 +0100

    average for product rate and only one rate by user who purchased the product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,23 +3,51 @@
 namespace App\Controller;
 
 use App\Form\PostReviewType;
-use App\Repository\ProductRepository;
 use App\Repository\ReviewRepository;
+use App\Repository\ProductRepository;
+use App\Repository\ProductOrderRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends AbstractController
 {
     #[Route('/product/{idProduct}', name: 'product')]
-    public function index(int $idProduct, ProductRepository $productRepository, ReviewRepository $reviewRepository, Request $request): Response
-    {
+    public function index(
+        int $idProduct,
+        ProductRepository $productRepository,
+        ReviewRepository $reviewRepository,
+        ProductOrderRepository $productOrderRepository
+    ): Response {
+        // display review form or not
+        $userReviews = $reviewRepository->findBy(['user' => $this->getUser(), 'product' => $idProduct]);
+        $purchased = false;
+        foreach($productOrderRepository->findBy(['product' => $idProduct]) as $productOrder) {
+            if ($productOrder->getCustomerOrder()->getUser() === $this->getUser()) {
+                $purchased = true;
+                break;
+            }
+        }
         $reviewForm = $this->createForm(PostReviewType::class)->createView();
+
+        // review average
+        $reviews = $reviewRepository->findBy(['product' => $idProduct]);
+        $sum = 0;
+        foreach($reviews as $review) {
+            $sum += $review->getRate();
+        }
+        if (count($reviews) > 0) {
+            $averageRate = $sum / count($reviews);
+        }
+
+        //var_dump($purchased, count($userReviews) == 0); die;
+
         return $this->render('product/index.html.twig', [
             'product' => $productRepository->find($idProduct),
             'review_form' => $reviewForm,
+            'display_review_form' => $purchased && count($userReviews) == 0,
             'reviews' => $reviewRepository->findByProduct($productRepository->find($idProduct)),
+            'average_rate' => count($reviews) ? $averageRate : null,
         ]);
     }
 }
