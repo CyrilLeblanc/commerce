45651af93ad146137c38c2b2e6f94032d0e45808
Cyril Leblanc <cyril_leblanc@outlook.fr>
2022-03-24 20:11:20 +0100

    fix fixtures

--- src/DataFixtures/Product.php
+++ src/DataFixtures/Product.php
@@ -4,17 +4,19 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use joshtronic\LoremIpsum;
 
 class Product extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        $lorem = new LoremIpsum();
         $categories = $manager->getRepository(\App\Entity\Category::class)->findAll();
         for ($i = 0; $i < 100; $i++) {
             $product = new \App\Entity\Product();
             $product->setName('Product ' . $i);
             $product->setPrice(rand(1, 1000) / 100);
-            $product->setDetail('lorem ipsum');
+            $product->setDetail($lorem->words(30));
             $manager->persist($product);
             for ($j = 0; $j < rand(1, 3); $j++) {
                 $product->addCategory($categories[rand(0, count($categories) - 1)]);
